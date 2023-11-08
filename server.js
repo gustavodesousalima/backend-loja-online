c01a328e5232362e24fa46878f10c565d25190d8
gustavodesousalima <gustavodesousalima918@gmail.com>
2023-11-08 17:48:35 -0300

    updated package

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); // Importe o módulo CORS
 
 require('dotenv').config();
-require("./db")
+require('./db');
 
 const port = process.env.PORT || 3000;
 
-const pictureRouter = require("./routes/pictureRoute")
+const pictureRouter = require('./routes/pictureRoute');
 
-app.use("/pictures", pictureRouter)
+// Configure o CORS no nível do aplicativo
+app.use(cors());
+
+app.use('/pictures', pictureRouter);
 
 app.listen(port, () => {
-    console.log(`O servidor está rodando na porta ${port}`)
+  console.log(`O servidor está rodando na porta ${port}`);
 });
