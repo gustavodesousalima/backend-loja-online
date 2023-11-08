9ef620779c422f665e4ac5540bcad1cdfd0313c4
gustavodesousalima <gustavodesousalima918@gmail.com>
2023-11-08 18:36:09 -0300

    updated package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors'); // Importe o módulo CORS
+const cors = require('cors');
+const path = require('path'); // Importe o módulo path
 
 require('dotenv').config();
 require('./db');
@@ -9,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 const pictureRouter = require('./routes/pictureRoute');
 
-app.use(express.static(path.join(__dirname, "uploads")))
+app.use(express.static(path.join(__dirname, 'uploads'))); // Utilize o módulo path para manipulação de caminhos
 
 // Configure o CORS no nível do aplicativo
 app.use(cors());
